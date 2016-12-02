5587ae1498865a50ad1536c31926d0767c731c3d
Natesh Narain <nnaraindev@gmail.com>
2016-12-01 22:33:16 -0500

    added setup.py and requirements file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gameboycore-python",
+    version="0.0.0",
+    packages=find_packages(),
+    scripts=[],
+
+    install_requires=[],
+
+    #
+    author="Natesh Narain",
+    author_email="nnaraindev@gmail.com",
+    description="Python bindings for Gameboy Core",
+    license="MIT",
+    keywords="gameboy emulator emulation",
+    url="https://github.com/nnarain/gameboycore-python"
+)
