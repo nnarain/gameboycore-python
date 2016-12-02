92304de7ae080333dc8c404cc0234ce5542ece27
Natesh Narain <nnaraindev@gmail.com>
2016-12-01 23:05:37 -0500

    wrapper file

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ import ez_setup
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
+from setuptools.extension import Extension
 
 setup(
     name="gameboycore-python",
---
+++ src/gameboycore/gameboycore_python.cpp
@@ -0,0 +1,9 @@
+
+/**
+    \file gameboycore_python.cpp
+    \brief Gameboy Core Python Wrapper
+    \author Natesh Narain <nnaraindev@gmail.com>
+    \date Dec 1 2016
+*/
+
+#include <boost/python.hpp>
