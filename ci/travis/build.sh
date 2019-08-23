05c50259ae614cea58eb401a4dbedbffebbd22b3
Natesh Narain <nnaraindev@gmail.com>
2019-08-22 22:53:35 -0400

    Resolves 26. Build manylinux wheel files.

---
+++ ci/travis/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements.txt
+
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install gameboycore --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" -v --exe -w /io/tests)
+done
